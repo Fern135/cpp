7bd9eceb242a42f454b296ec639bb5e738655eb7
Abraham <abraham@19cah.com>
2018-02-07 11:38:09 -0500

    add solution for problem 3

---
+++ 1-10/SumOfTwoNumbers.cpp
@@ -0,0 +1,27 @@
+/*
+
+Write a program that stores the integers 62 and 99 in variables,
+and stores the sum of these two in a variable named total.
+Display the total on the screen.
+
+Author: Carlos Abraham, @19cah
+www.19cah.com
+
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a = 62;
+	int b = 99;
+	int total = a + b;
+
+	cout << "Total: " << total << endl;
+	
+	/* Are you using Windows OS? -yes uncomment the below code */
+	system("pause");
+	return 0;
+}
